8c59b3d489cb46ab8d11e9c46d48fb2c07f424aa
No Name <no-name-github@nexjoin.com>
2016-02-14 01:05:55 -0800

    Create simpleMain.js

---
+++ js/simpleMain.js
@@ -0,0 +1,28 @@
+ //Main AngularJS Web Application
+var app = angular.module('tutorialWebApp', [
+  'ngRoute'
+]);
+
+//Configure the Routes
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "tab/lobby.html", controller: "PageCtrl"})
+    .when("/about", {templateUrl: "tab/singleTab.html", controller: "PageCtrl"})
+}]);
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
